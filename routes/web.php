357302e491ddcee6b10096e2586d34c4781495b1
”nezu” <tonight.tonight.018@gmail.com>
2020-05-23 08:43:17 +0000

    text 13

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 Route::get('XXX', 'Admin\AAAController@bbb');
 
 
-Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware(‘auth’);
+Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware('auth');
+Route::post('admin/profile/create', 'Admin\ProfileController@create');
 
 
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware(‘auth’);
-
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+Route::post('admin/profile/edit', 'Admin\ProfileController@update');
 
 Auth::routes();
 
