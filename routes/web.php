a377ff54c6542cd0a8a89141fa5710c9ec859257
”nezu” <tonight.tonight.018@gmail.com>
2020-05-31 09:57:00 +0000

    14 課題修正コミット

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,11 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
 });
 
 Route::get('XXX', 'Admin\AAAController@bbb');
@@ -33,3 +36,5 @@ Route::post('admin/profile/edit', 'Admin\ProfileController@update');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
